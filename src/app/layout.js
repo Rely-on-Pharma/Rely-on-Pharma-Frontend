c1b78956d9b0189c2eeebf6cc67887369dd58633
Shreyash <shreyash.22010681@viit.ac.in>
2024-02-15 22:45:56 +0530

    Added persistent user with context

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import DrawerAppBar from "@/common/layout/Navbar/Navbar";
 import Footer from "../common/layout/Footer/Footer.jsx";
 import AppContextProvider from "@/constants/context/contextProvider";
+import { MemoizedSnackBar } from "@/constants/SDK/CustomSnackBar";
 export const metadata = {
   title: "Next App",
   description: "Next.js starter app",
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <AppContextProvider>
+          <MemoizedSnackBar/>
           <ThemeRegistry>
             <header>
               <DrawerAppBar />
--- src/constants/context/contextProvider.js
+++ src/constants/context/contextProvider.js
@@ -1,26 +1,58 @@
-import React, { useReducer } from "react";
+"use client";
+import React, { useEffect, useReducer } from "react";
 import AppContext from "./context";
 import AppReducer, { initialState } from "./contextReducer";
 
 const AppContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  //   useeffects to handle user auth
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      dispatch({ type: "LOGIN", payload: { user: JSON.parse(storedUser) } });
+    }
+  }, []);
+
+  // Update localStorage when the user state changes
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
   const value = {
     // snackbar state values
-    openSnackBar: state.snackBar.open,
-    snackBarMessage: state.snackBar.message,
-    snackBarType: state.snackBar.severity,
-
+    snackBar: {
+      open: state.snackBar.open,
+      message: state.snackBar.message,
+      severity: state.snackBar.severity,
+    },
+    //auth user
+    user: {
+      isAuthenticated: state.user.isAuthenticated,
+      data: state.user.data,
+    },
     // functions
     // snackbar functions
     showSnackbar: (snackBarMessage, snackBarType) => {
       dispatch({
         type: "SHOW_SNACKBAR",
-        payload: { message:snackBarMessage, severity:snackBarType },
+        payload: { message: snackBarMessage, severity: snackBarType },
       });
     },
     hideSnackbar: () => {
       dispatch({ type: "HIDE_SNACKBAR" });
+    },
+
+    //auth functions
+    loginUser: (user) => {
+      dispatch({
+        type: "LOGIN",
+        payload: { user },
+      });
+    },
+    logOutUser: () => {
+      dispatch({
+        type: "LOGOUT",
+      });
     },
   };
 
@@ -33,8 +65,6 @@ const AppContextProvider = ({ children }) => {
 };
 
 export default AppContextProvider;
-
-
 
 // we will call the functions and values in our component from here like showsnackbar from here we will dispatch reducer function with type and payload
 // this will make a call to our reducer where we will update our global state variables we will match which one function to call based on type and can access the payload.
