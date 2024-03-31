6d39ab245e49960ae01592732602bb83533ee652
Alok Joshi <AlokJoshi523@gmail.com>
2024-03-31 13:12:05 +0530

    Add formatting

--- src/app/(customer)/product/[name]/page.jsx
+++ src/app/(customer)/product/[name]/page.jsx
@@ -1,80 +1,81 @@
-"use client"
-import DescriptionTab from '@/pages/Product/DescriptionTab'
-import Gallery from '@/pages/Product/Gallery'
-import ProdDetails from '@/pages/Product/ProdDetails'
-import SimilarComponents from '@/pages/Product/SimilarComponents'
-import { productData } from '@/constants/data/productData.js'
-import { Box, Grid, Typography, styled } from '@mui/material'
-import React, {useEffect,useState} from 'react'
-import { useMobile } from '@/common/utils/finndViewSize'
-import { useRouter , useSearchParams, usePathname } from 'next/navigation';
+"use client";
+import DescriptionTab from "@/pages/Product/DescriptionTab";
+import Gallery from "@/pages/Product/Gallery";
+import ProdDetails from "@/pages/Product/ProdDetails";
+import SimilarComponents from "@/pages/Product/SimilarComponents";
+import { productData } from "@/constants/data/productData.js";
+import { Box, Grid, Typography, styled } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useMobile } from "@/common/utils/finndViewSize";
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
 const ProductData = {
-    "id": "65d477565b75282dda587003",
-    "name": "Unbranded Concrete Mouse",
-    "company":"rely-on-pharma",
-    "desc":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat sed cras ornare arcu. Tristique senectus et netus et malesuada fames ac. Semper quis lectus nulla at volutpat diam. Nunc faucibus a pellentesque sit amet porttitor.",
-    "use":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro optio aliquam tempore quaerat quos, nam totam unde adipisci vitae? Neque necessitatibus, tenetur obcaecati, blanditiis eius accusantium ipsa esse excepturi similique corporis tempore vitae, quae odio.",
-    "ingredients":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro optio aliquam tempore quaerat quos, nam totam unde adipisci vitae? Neque necessitatibus, tenetur obcaecati, blanditiis eius accusantium ipsa esse excepturi similique corporis tempore vitae, quae odio.", 
-    "mainImg": "https://loremflickr.com/640/480?lock=2391645746102272",
-    "imgs": [
-      {
-        "id": "65d477565b75282dda587004",
-        "url": "https://loremflickr.com/640/480?lock=6586178289532928"
-      },
-      {
-        "id": "65d477565b75282dda587005",
-        "url": "https://picsum.photos/seed/9o1M7S/640/480"
-      },
-      {
-        "id": "65d477565b75282dda587006",
-        "url": "https://picsum.photos/seed/1mUkGldOY/640/480"
-      },
-      {
-        "id": "65d477565b75282dda587007",
-        "url": "https://picsum.photos/seed/OyKk2mm/640/480"
-      },
-      {
-        "id": "65d477565b75282dda587008",
-        "url": "https://picsum.photos/seed/qebwUfV/640/480"
-      }
-    ],
-    "price": "904.00",
-    "category": "Child",
-    "discount":"40",
-   }
-
-  const CustomSingleProduct = styled(Box)(({theme})=>({
-    padding:"4rem", 
-    ".productContainer":{
-      width:"90%",
-      margin:"auto"
+  id: "65d477565b75282dda587003",
+  name: "Unbranded Concrete Mouse",
+  company: "rely-on-pharma",
+  desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat sed cras ornare arcu. Tristique senectus et netus et malesuada fames ac. Semper quis lectus nulla at volutpat diam. Nunc faucibus a pellentesque sit amet porttitor.",
+  use: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro optio aliquam tempore quaerat quos, nam totam unde adipisci vitae? Neque necessitatibus, tenetur obcaecati, blanditiis eius accusantium ipsa esse excepturi similique corporis tempore vitae, quae odio.",
+  ingredients:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro optio aliquam tempore quaerat quos, nam totam unde adipisci vitae? Neque necessitatibus, tenetur obcaecati, blanditiis eius accusantium ipsa esse excepturi similique corporis tempore vitae, quae odio.",
+  mainImg: "https://loremflickr.com/640/480?lock=2391645746102272",
+  imgs: [
+    {
+      id: "65d477565b75282dda587004",
+      url: "https://loremflickr.com/640/480?lock=6586178289532928",
     },
+    {
+      id: "65d477565b75282dda587005",
+      url: "https://picsum.photos/seed/9o1M7S/640/480",
+    },
+    {
+      id: "65d477565b75282dda587006",
+      url: "https://picsum.photos/seed/1mUkGldOY/640/480",
+    },
+    {
+      id: "65d477565b75282dda587007",
+      url: "https://picsum.photos/seed/OyKk2mm/640/480",
+    },
+    {
+      id: "65d477565b75282dda587008",
+      url: "https://picsum.photos/seed/qebwUfV/640/480",
+    },
+  ],
+  price: "904.00",
+  category: "Child",
+  discount: "40",
+};
 
-    [theme.breakpoints.down("md")]:{
-      padding:"0.5rem", 
-      ".productContainer":{
-        width:"100%",
-      }
-    }
-  }))
+const CustomSingleProduct = styled(Box)(({ theme }) => ({
+  padding: "4rem",
+  ".productContainer": {
+    width: "90%",
+    margin: "auto",
+  },
+
+  [theme.breakpoints.down("md")]: {
+    padding: "0.5rem",
+    ".productContainer": {
+      width: "100%",
+    },
+  },
+}));
 const getId = (pathname) => {
-    const regex = /\/product\/([^/]+)/;
-    const match = pathname.match(regex);
+  const regex = /\/product\/([^/]+)/;
+  const match = pathname.match(regex);
 
-    if (match && match.length > 1) {
-      const idValue = match[1];
-      return idValue
-    } else {
-        return ""
-    }
-}
+  if (match && match.length > 1) {
+    const idValue = match[1];
+    return idValue;
+  } else {
+    return "";
+  }
+};
 const SingleProduct = () => {
-  const isMobile = useMobile()
-  const pathname = usePathname()
-  const id  = getId(pathname)
+  const isMobile = useMobile();
+  const pathname = usePathname();
+  const id = getId(pathname);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [productData, setProductData] = useState([])
+  const [productData, setProductData] = useState([]);
 
   useEffect(() => {
     // Function to fetch data from the API
@@ -82,7 +83,7 @@ const SingleProduct = () => {
       try {
         const response = await fetch(`http://localhost:8000/products/${id}`);
         if (!response.ok) {
-          throw new Error('Failed to fetch data');
+          throw new Error("Failed to fetch data");
         }
         const jsonData = await response.json();
         setProductData(jsonData);
@@ -111,26 +112,35 @@ const SingleProduct = () => {
   }
   return (
     <CustomSingleProduct>
-      <Grid className='productContainer' container spacing={2}>
-      {/* Gallery */}
-      <Grid item xs={12} style={{padding:"0"}}  md={6}><Gallery  imageGallery={ProductData?.imgs}/></Grid>
-      <Grid item xs={12} style={{padding:"0"}} md={6}><ProdDetails productId = {id} productData={productData}/></Grid>
-      {/* Pricing */}
-        
+      <Grid className="productContainer" container spacing={2}>
+        {/* Gallery */}
+        <Grid item xs={12} style={{ padding: "0" }} md={6}>
+          <Gallery imageGallery={ProductData?.imgs} />
+        </Grid>
+        <Grid item xs={12} style={{ padding: "0" }} md={6}>
+          <ProdDetails productId={id} productData={productData} />
+        </Grid>
+        {/* Pricing */}
       </Grid>
       {/* details */}
-      <DescriptionTab desc={productData?.description} howToUse={productData?.how_to_use} ingredients={"sample ingredient list"}/>
+      <DescriptionTab
+        desc={productData?.description}
+        howToUse={productData?.how_to_use}
+        ingredients={"sample ingredient list"}
+      />
       {/* related */}
 
-      <Typography variant='h3'>Similar Products</Typography>
-      <SimilarComponents slidesPerView={isMobile ? 1 : 3}
-  spaceBetween={30}
-  loop = {true}
-  delay = {2500}
-  data={productData}
-  navigation = {false}/>
+      <Typography variant="h3">Similar Products</Typography>
+      <SimilarComponents
+        slidesPerView={isMobile ? 1 : 3}
+        spaceBetween={30}
+        loop={true}
+        delay={2500}
+        data={productData}
+        navigation={false}
+      />
     </CustomSingleProduct>
-  )
-}
+  );
+};
 
-export default SingleProduct
+export default SingleProduct;
--- src/pages/Product/ProdDetails.jsx
+++ src/pages/Product/ProdDetails.jsx
@@ -1,5 +1,13 @@
 "use client";
-import { Box, FormControlLabel, IconButton, Radio, RadioGroup, Typography, styled } from "@mui/material";
+import {
+  Box,
+  FormControlLabel,
+  IconButton,
+  Radio,
+  RadioGroup,
+  Typography,
+  styled,
+} from "@mui/material";
 import React, { useContext, useState, useEffect } from "react";
 import StarIcon from "@mui/icons-material/Star";
 import { MemoizedButton } from "@/constants/SDK/CustomButton";
@@ -15,54 +23,53 @@ const CustomPRodDetails = styled(Box)(({ theme }) => ({
     textAlign: "left",
     textTransform: "capitalize",
   },
-  ".btn":{
-    textTransform:"capitalize",
-    borderRadius:"8px",
-    "&.addToBtn":{
-      background:colors?.primaryDark,
-      color:colors?.MonochromeLight,
-      width:"40%",
-      padding:"8px"
+  ".btn": {
+    textTransform: "capitalize",
+    borderRadius: "8px",
+    "&.addToBtn": {
+      background: colors?.primaryDark,
+      color: colors?.MonochromeLight,
+      width: "40%",
+      padding: "8px",
     },
-    "&.favBtn":{
-      background:colors?.secondaryMedium,
-      color:colors?.white,
-      width:"8%",
-      padding:"10px",
-      "&.active":{
-        background:colors?.secondaryDark,
-    color:colors?.white,
-      }
+    "&.favBtn": {
+      background: colors?.secondaryMedium,
+      color: colors?.white,
+      width: "8%",
+      padding: "10px",
+      "&.active": {
+        background: colors?.secondaryDark,
+        color: colors?.white,
+      },
     },
-    "&.checkBtn":{
-      background:colors?.secondaryMedium,
-      color:colors?.white,
-      padding:"8px",
-      width:"50%",
-    }
+    "&.checkBtn": {
+      background: colors?.secondaryMedium,
+      color: colors?.white,
+      padding: "8px",
+      width: "50%",
+    },
   },
 
-  [theme.breakpoints.down("md")]:{
+  [theme.breakpoints.down("md")]: {
     padding: "0.5rem",
     ".prodName": {
       fontSize: "26px",
     },
-    ".btn":{
-      "&.addToBtn":{
-        width:"80%",
+    ".btn": {
+      "&.addToBtn": {
+        width: "80%",
       },
-      "&.favBtn":{
-        width:"16%",
-        
+      "&.favBtn": {
+        width: "16%",
       },
-      "&.checkBtn":{
-        width:"100%",
-      }
+      "&.checkBtn": {
+        width: "100%",
+      },
     },
-  }
+  },
 }));
-const ProdDetails = ({ productData , productId}) => {
-  const {addToCart} = useContext(AppContext)
+const ProdDetails = ({ productData, productId }) => {
+  const { addToCart } = useContext(AppContext);
   const [selectedOption, setSelectedOption] = useState("quantity");
   const [quantity, setQuantity] = useState(1); // Default quantity for the quantity option
   const [selectedPack, setSelectedPack] = useState("2"); // Default selected pack option
@@ -75,9 +82,11 @@ const ProdDetails = ({ productData , productId}) => {
     // Function to fetch data from the API
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://localhost:8000/variants/${productId}`);
+        const response = await fetch(
+          `http://localhost:8000/variants/${productId}`,
+        );
         if (!response.ok) {
-          throw new Error('Failed to fetch data');
+          throw new Error("Failed to fetch data");
         }
         const jsonData = await response.json();
         setVariants(jsonData);
@@ -97,56 +106,69 @@ const ProdDetails = ({ productData , productId}) => {
     };
   }, []); // Empty dependency array means this effect runs only once, similar to componentDidMount
 
-  
   const handleAddToCart = () => {
     addToCart(productData, quantity);
   };
 
   const calDisPrice = (price, discount) => {
-    return (price - ((price*discount)/100))
+    return price - (price * discount) / 100;
   };
   const decrementQuantity = () => {
-    setQuantity(prevQuantity => Math.max(prevQuantity - 1, 1)); // Ensure quantity doesn't go below 1
+    setQuantity((prevQuantity) => Math.max(prevQuantity - 1, 1)); // Ensure quantity doesn't go below 1
   };
 
   const incrementQuantity = () => {
-    setQuantity(prevQuantity => prevQuantity + 1);
+    setQuantity((prevQuantity) => prevQuantity + 1);
   };
-  const returnPriceComponent = (price,discount) => {
-
-      if(discount){
-
-          return (
-      <Typography style={{ marginBlock: "8px", fontSize:"20px",color:colors?.primaryDark, fontWeight:"700"}}>
+  const returnPriceComponent = (price, discount) => {
+    if (discount) {
+      return (
         <Typography
-          variant="span"
-          style={{ textDecorationLine: discount && "line-through", marginRight: "12px", color:colors?.MonochromeMedium, fontWeight:"600", fontSize:"16px"}}
+          style={{
+            marginBlock: "8px",
+            fontSize: "20px",
+            color: colors?.primaryDark,
+            fontWeight: "700",
+          }}
         >
-         
+          <Typography
+            variant="span"
+            style={{
+              textDecorationLine: discount && "line-through",
+              marginRight: "12px",
+              color: colors?.MonochromeMedium,
+              fontWeight: "600",
+              fontSize: "16px",
+            }}
+          >
+            Rs. {price}
+          </Typography>
+          Rs. {calDisPrice(productData?.price, discount)}
+        </Typography>
+      );
+    } else {
+      return (
+        <Typography
+          style={{
+            marginBlock: "8px",
+            fontSize: "20px",
+            color: colors?.primaryDark,
+            fontWeight: "700",
+          }}
+        >
           Rs. {price}
         </Typography>
-        Rs. {calDisPrice(productData?.price,discount)}
-      </Typography>
-        )
-
+      );
     }
-    else{
-    return (
-        <Typography style={{ marginBlock: "8px", fontSize:"20px",color:colors?.primaryDark, fontWeight:"700"}}>
-          Rs. {price}
-      </Typography>
-    )
+  };
 
-    }
-}
-
- if (loading) {
+  if (loading) {
     return <div>Loading...</div>;
   }
 
   if (error) {
     return <div>Error: {error}</div>;
-  }      
+  }
   return (
     <CustomPRodDetails>
       <Typography className="prodName">{productData?.name}</Typography>
@@ -157,17 +179,15 @@ const ProdDetails = ({ productData , productId}) => {
           marginBlock: "8px",
         }}
       >
-      by {productData?.brand}
+        by {productData?.brand}
       </Typography>
-      {
-          /* <Typography style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+      {/* <Typography style={{ display: "flex", alignItems: "center", gap: "4px" }}>
         <StarIcon /> 4.5
        </Typography>
-        */
-      }
+        */}
 
-      {returnPriceComponent(productData?.price,discount)}
-    <RadioGroup
+      {returnPriceComponent(productData?.price, discount)}
+      <RadioGroup
         aria-label="quantity-option"
         name="quantity-option"
         value={selectedOption}
@@ -175,58 +195,74 @@ const ProdDetails = ({ productData , productId}) => {
           setSelectedOption(e.target.value);
           if (e.target.value === "pack") {
             setQuantity(1); // Reset quantity to default if pack option is selected
-          }
-          else if(e.target.value == "quantity"){
-
-              setDiscount(0);
-              setQuantity(0);
+          } else if (e.target.value == "quantity") {
+            setDiscount(0);
+            setQuantity(0);
           }
         }}
         style={{ marginBlock: "16px" }}
       >
-        <FormControlLabel value="quantity" control={<Radio />} label="Select by quantity" /> 
+        <FormControlLabel
+          value="quantity"
+          control={<Radio />}
+          label="Select by quantity"
+        />
         {selectedOption === "quantity" && (
-        <Box style={{ display: "flex", alignItems: "center", gap: "12px" }}>
-          {/* Quantity counter */}
-          <IconButton onClick={decrementQuantity} className="btn">
-            -
-          </IconButton>
-          <Typography>{quantity}</Typography>
-          <IconButton onClick={incrementQuantity} className="btn">
-            +
-          </IconButton>
-        </Box>
-      )}
-        <FormControlLabel value="pack" control={<Radio />} label="Select pack of" />
+          <Box style={{ display: "flex", alignItems: "center", gap: "12px" }}>
+            {/* Quantity counter */}
+            <IconButton onClick={decrementQuantity} className="btn">
+              -
+            </IconButton>
+            <Typography>{quantity}</Typography>
+            <IconButton onClick={incrementQuantity} className="btn">
+              +
+            </IconButton>
+          </Box>
+        )}
+        <FormControlLabel
+          value="pack"
+          control={<Radio />}
+          label="Select pack of"
+        />
         {selectedOption === "pack" && (
-        <Box style={{ display: "flex", alignItems: "center", gap: "12px" }}>
+          <Box style={{ display: "flex", alignItems: "center", gap: "12px" }}>
             {console.log(variants)}
-          {/* Your pack options */}
-           { variants.map((variant) => {
-               return <MemoizedButton
-            key = {variant.variant_id}
-            className={`btn favBtn ${selectedPack === "2" ? "active" : ""}`}
-            content={variant.quantity}
-            handleClick={() => {
-              setQuantity(variant.quantity);
-              setSelectedPack(variant.quantity.toString());
-              setDiscount(variant.discount_percentage);
-            }}
-          />
-
-            })
-           }
-          
-        </Box>
-      )}
+            {/* Your pack options */}
+            {variants.map((variant) => {
+              return (
+                <MemoizedButton
+                  key={variant.variant_id}
+                  className={`btn favBtn ${selectedPack === "2" ? "active" : ""}`}
+                  content={variant.quantity}
+                  handleClick={() => {
+                    setQuantity(variant.quantity);
+                    setSelectedPack(variant.quantity.toString());
+                    setDiscount(variant.discount_percentage);
+                  }}
+                />
+              );
+            })}
+          </Box>
+        )}
       </RadioGroup>
 
-      
-
-     
-      <Box style={{ marginBlock: "16px", width:"100%", display:"flex",alignItems:"center", gap:"12px" }}>
-        <MemoizedButton className={"btn addToBtn"} content={"Add to Cart"} handleClick={handleAddToCart}/>
-        <IconButton className="btn favBtn"><FavoriteIcon  /></IconButton>
+      <Box
+        style={{
+          marginBlock: "16px",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          gap: "12px",
+        }}
+      >
+        <MemoizedButton
+          className={"btn addToBtn"}
+          content={"Add to Cart"}
+          handleClick={handleAddToCart}
+        />
+        <IconButton className="btn favBtn">
+          <FavoriteIcon />
+        </IconButton>
       </Box>
       <MemoizedButton className={"btn checkBtn"} content={"checkout"} />
     </CustomPRodDetails>
