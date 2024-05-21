68ec1775071598aa170670885de725fb85fc74d5
Shreyash <shreyash.22010681@viit.ac.in>
2024-05-21 15:24:50 +0530

    Fixed next config for images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["logos-world.net","www.hismileteeths.us","loremflickr.com", "picsum.photos", "images.unsplash.com",`${process.env.AWS_BUCKET}`], //make it 'your-domain.com'
+        domains: ["logos-world.net","www.hismileteeths.us","loremflickr.com", "picsum.photos", "images.unsplash.com","relyonpharma.s3.ap-south-1.amazonaws.com"], //make it 'your-domain.com'
       },
 }
 
